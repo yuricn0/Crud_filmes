3b0c4d30cee3c1e52f95df0eeef6ec7015d7fe07
Yuri Nunes <yuri.cruz10@hotmail.com>
2025-02-02 16:29:35 -0300

    first commit

---
+++ src/main/java/br/com/ydcns/AppFilmes/AppFilmesApplication.java
@@ -0,0 +1,13 @@
+package br.com.ydcns.AppFilmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppFilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppFilmesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ydcns/AppFilmes/AppFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ydcns.AppFilmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppFilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
