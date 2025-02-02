0ce0107b7e792dea2c912e777828871d8f727bf2
Yuri Nunes <yuri.cruz10@hotmail.com>
2025-02-02 16:57:10 -0300

    Entity criada com os construtores e os Getters e Setters

--- src/main/java/br/com/ydcns/AppFilmes/model/ListaFilmes.java
+++ src/main/java/br/com/ydcns/AppFilmes/model/ListaFilmes.java
@@ -23,5 +23,49 @@ public class ListaFilmes {
 	
 	@Column(nullable = false)
 	private Integer anoLancamento;
+	
+	public ListaFilmes( ) {}
 
+	public ListaFilmes(Long id, String titulo, String genero, Integer anoLancamento) {
+		this.id = id;
+		this.titulo = titulo;
+		this.genero = genero;
+		this.anoLancamento = anoLancamento;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public Integer getAnoLancamento() {
+		return anoLancamento;
+	}
+
+	public void setAnoLancamento(Integer anoLancamento) {
+		this.anoLancamento = anoLancamento;
+	}
+	
+	
+	
+	
 }
